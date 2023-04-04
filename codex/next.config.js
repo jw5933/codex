a4a6e336f0b4b349f1f5180d869264130355f137
jw5933 <jw5933@nyu.edu>
2023-04-04 13:21:12 -0400

    re add next config env

--- codex/next.config.js
+++ codex/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    BACKEND_SERVER: process.env.BACKEND_SERVER,
+    // Add additional environment variables here as needed
+  },
 };
 
 module.exports = nextConfig;
